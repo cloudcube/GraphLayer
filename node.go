52152f4646635aef07a53278f6dbeba6f9feb033
haibinpark <zhaohaibin007@gmail.com>
2013-03-22 07:12:38 +0800

    完成节点查找和删除

--- node.go
+++ node.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"strconv"
 )
 
 // type NodeInterface interface {
@@ -11,12 +12,13 @@ import (
 // 	AddNode(data map[string]string) (*GraphDataTemplate, error)
 
 // 	// 获取节点
-// 	GetNode() (*GraphDataTemplate, error)
+// 	GetNode(nodeId uint64) (*GraphDataTemplate, error)
 
 // 	// 删除节点
-// 	DeleteNode() error
+// 	DeleteNode(nodeId uint64) error
 // }
 
+//创建节点
 func (session *Session) CreateNode(data map[string]string) (nodeTempalte *GraphDataTemplate, err error) {
 	s, err := json.Marshal(data)
 	if err != nil {
@@ -37,4 +39,43 @@ func (session *Session) CreateNode(data map[string]string) (nodeTempalte *GraphD
 		400: errors.New("Invalid data sent."),
 	}
 	return template[0], session.NewError(errorList)
+}
+
+// 获取节点
+func (session *Session) GetNode(nodeId uint64) (nodeTemplate *GraphDataTemplate, err error) {
+	if nodeId < 0 {
+		return nodeTemplate, errors.New("Invalid node id specified.")
+	}
+	session.Method = "get"
+	url := session.URL + "/node/"
+	body, err := session.Send(url+strconv.FormatUint(uint64(nodeId), 10), "") // convert uint -> string and send http request
+	if err != nil {
+		return nodeTemplate, err
+	}
+	template, err := session.Unmarshal(body) // json.Unmarshal wrapper with some type assertions etc
+	if err != nil {
+		return nodeTemplate, err
+	}
+	errorList := map[int]error{
+		404: errors.New("Node not found."),
+	}
+	return template[0], session.NewError(errorList)
+}
+
+// 删除节点
+func (session *Session) DeleteNode(nodeId uint64) error {
+	node, err := session.GetNode(nodeId) // find properties for node
+	if err != nil {
+		return err
+	}
+	session.Method = "delete"
+	_, err = session.Send(node.Self, "")
+	if err != nil {
+		return err
+	}
+	errorList := map[int]error{
+		404: errors.New("Node not found."),
+		409: errors.New("Unable to delete node. May still have relationships."),
+	}
+	return session.NewError(errorList)
 }
--- node_test.go
+++ node_test.go
@@ -1,11 +1,17 @@
 package graphdb
 
 import (
+	"log"
 	"testing"
 )
 
+var ID uint64
+
+var settingFile = "databaseConfig.json"
+
 func TestCreateNode(t *testing.T) {
-	session, err := Dial("databaseConfig.json")
+	log.Println("Start testing CreateNode function")
+	session, err := Dial(settingFile)
 	if err != nil {
 		t.Error(err)
 	}
@@ -16,5 +22,34 @@ func TestCreateNode(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	t.Log(node)
+	ID = node.ID
+	log.Println(node)
+	log.Println("Create Node Sucessful!")
+}
+
+func TestGetNode(t *testing.T) {
+	log.Println("Start testing GetNode function")
+	session, err := Dial(settingFile)
+	if err != nil {
+		t.Error(err)
+	}
+	data, err := session.GetNode(ID)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(data)
+	log.Println("Get Node Sucessful")
+}
+
+func TestDeleteNode(t *testing.T) {
+	log.Println("Start testing DeleteNode function")
+	session, err := Dial(settingFile)
+	if err != nil {
+		t.Error(err)
+	}
+	err = session.DeleteNode(ID)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println("Delete Node Sucessful.")
 }
