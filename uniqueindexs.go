861bf83d4c8012fe5cb9bf51c2838ea1f79222e9
david <david@cloudcube.net>
2013-04-15 07:06:00 +0800

    fix bug [CreateUniqueNode]

--- uniqueindexs.go
+++ uniqueindexs.go
@@ -3,36 +3,37 @@ package graphdb
 import (
 	"encoding/json"
 	"errors"
-	"log"
+	// "log"
 )
 
 // 创建唯一节点
-func (session *Session) CreateUniqueNode(indexName string, uniqueness string, data map[string]interface{}) (result *GraphDataTemplate, err error) {
+func (session *Session) CreateUniqueNode(indexName string /*uniqueness string,*/, data map[string]interface{}) (result *GraphDataTemplate, err error) {
 	session.Method = "post"
 	url := session.URL + "/" + "index" + "/" + "node"
 	if len(indexName) == 0 {
 		return result, errors.New("IndexName is nil!")
 	}
 	url += "/" + indexName
-	if len(uniqueness) == 0 {
-		return result, errors.New("Uniquenes is nil")
-	}
-	url += "?" + "uniqueness=" + uniqueness
-	log.Println(url)
+	// if len(uniqueness) == 0 {
+	// 	return result, errors.New("Uniquenes is nil")
+	// }
+	//url += "?" + "uniqueness=" + uniqueness
+	url += "?unique"
+	// log.Println(url)
 	if data == nil {
 		return result, errors.New("Node Infor is nil!")
 	}
-	log.Println(data)
+	// log.Println(data)
 	buf, err := json.Marshal(data)
 	if err != nil {
 		return result, err
 	}
-	log.Println(string(buf))
+	// log.Println(string(buf))
 	body, err := session.Send(url, string(buf))
 	if err != nil {
 		return result, err
 	}
-	log.Println(session.StatusCode)
+	// log.Println(session.StatusCode)
 	results, err := session.Unmarshal(body)
 	if err != nil {
 		return result, err
--- uniqueindexs_test.go
+++ uniqueindexs_test.go
@@ -12,55 +12,92 @@ func TestCreateUniqueNode(t *testing.T) {
 		t.Error(err)
 	}
 	indexName := "people"
-	// err = session.CreateNodeIndex(indexName)
 	indexType := "fulltext"
 	indexProvider := "lucene"
 	err = session.CreateNodeIndexWithConf(indexName, indexType, indexProvider)
 	if err != nil {
 		t.Error(err)
 	}
-	uniqueness := "create_or_fail"
-	// oneNode := map[string]interface{}{
-	// 	"name":     "Tobias",
-	// 	"sequence": 1,
-	// }
-	// data := map[string]interface{}{
-	// 	"key":        "name",
-	// 	"value":      "Tobias",
-	// 	"properties": oneNode,
-	// }
-	node1Data := map[string]interface{}{}
-	node1Data["name"] = "Peter"
-	node1Data["sequence"] = 2
-	node1, err := session.CreateNode(node1Data)
-	if err != nil {
-		t.Error(err)
+	// uniqueness := "unique"
+	oneNode := map[string]interface{}{
+		"name":     "Tobias",
+		"sequence": 2,
 	}
-	indexKey := "name"
-	indexValue := "Peter"
-
-	_, err = session.AddNodeToIndex(indexKey, indexValue, indexName, node1.ID)
-	if err != nil {
-		t.Error(err)
+	data := map[string]interface{}{
+		"key":        "name",
+		"value":      "Tobias",
+		"properties": oneNode,
 	}
-
-	data := map[string]interface{}{}
-	data["key"] = indexKey
-	data["value"] = indexValue
-	data["properties"] = node1Data
-	result, err := session.CreateUniqueNode(indexName, uniqueness, data)
+	log.Println("Create a unique node in an index")
+	result, err := session.CreateUniqueNode(indexName, data)
 	if err != nil {
 		t.Error(err)
 	}
 	log.Println(result)
-	// log.Println("clear data...")
-	// err = session.DeleteNode(node1.ID)
-	// if err != nil {
-	// 	t.Error(err)
-	// }
-	// err = session.DeleteNodeIndex(indexName)
-	// if err != nil {
-	// 	t.Error(err)
-	// }
+	log.Println("Create a unique node in an index (the case where it exists)")
+	data1 := map[string]interface{}{
+		"key":        "okokok",
+		"value":      "Exist Node Test.",
+		"properties": oneNode,
+	}
+	result, err = session.CreateUniqueNode(indexName, data1)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(result)
+	log.Println("Add a node to an index unless a node already exists for the given mapping")
+	nodeData := map[string]interface{}{
+		"name": "node002",
+		"key":  100,
+	}
+	node02, err := session.CreateNode(nodeData)
+	if err != nil {
+		t.Error(err)
+	}
+	data2 := map[string]interface{}{
+		"key":   "name",
+		"value": "Mattias",
+		"uri":   node02.Self,
+	}
+	result, err = session.CreateUniqueNode(indexName, data2)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(result)
+	log.Println("clear data ...")
+	err = session.DeleteNode(node02.ID)
+	if err != nil {
+		t.Error(err)
+	}
+	indexKey := "name"
+	indexValue := "Tobias"
+	results, err := session.FindNodeByMatch(indexName, indexKey, indexValue)
+	if err != nil {
+		t.Error(err)
+	}
+	for _, dataResult := range results {
+		log.Println(dataResult)
+		log.Println("node Id:")
+		log.Println(dataResult.ID)
+		err = session.DeleteNode(dataResult.ID)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	key := indexKey
+	err = session.DeleteNodeIndex(key)
+	if err != nil {
+		t.Error(err)
+	}
+	key = "okokok"
+	err = session.DeleteNodeIndex(key)
+	if err != nil {
+		t.Error(err)
+	}
+	err = session.DeleteNodeIndex(indexName)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println("data cleaned!")
 	log.Println("test CreateUniqueNode finshed!")
 }
